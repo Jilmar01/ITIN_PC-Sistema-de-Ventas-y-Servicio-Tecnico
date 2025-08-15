de95281d0a78dc95f86219fdbcb9da142cc26b0e
USER <USER@JILMAR>
2025-08-15 01:22:00 -0500

    Nuevos atributos y metodos en ReportesEmpleados

--- src/main/java/itin_pc/model/ReporteEmpleados.java
+++ src/main/java/itin_pc/model/ReporteEmpleados.java
@@ -3,10 +3,12 @@ package itin_pc.model;
 public class ReporteEmpleados extends Empleado {
 
     private int numeroVentas;
+    private double montoTotal;
 
-    public ReporteEmpleados(int id, String nombre, String apellido, int numeroVentas) {
+    public ReporteEmpleados(int id, String nombre, String apellido, int numeroVentas, double montoTotal) {
         super(id, nombre, apellido);
         this.numeroVentas = numeroVentas;
+        this.montoTotal = montoTotal;
     }
 
     public int getNumeroVentas() {
@@ -15,6 +17,14 @@ public class ReporteEmpleados extends Empleado {
 
     public void setNumeroVentas(int numeroVentas) {
         this.numeroVentas = numeroVentas;
+    }
+
+    public double getMontoTotal() {
+        return montoTotal;
+    }
+
+    public void setMontoTotal(double montoTotal) {
+        this.montoTotal = montoTotal;
     }
     
 }
